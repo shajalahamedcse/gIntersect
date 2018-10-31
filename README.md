61aac76e4462650cc423af8bdd1dcf4c7ea231dd
shajalahamedcse <shajalahamedcse@gmail.com>
2018-10-31 16:17:41 +0600

    done

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # gIntersect
+
+Only prints those words that are both in [File A] and [File B]
+
+
+    usage : intersect [File A] [File B]
+    Prints out the lines that are both in [File A] and [File B]
+
+
+#Example
+
+    $ cat FileA.txt
+    hello
+    $ cat FileB.txt
+    hello
+    world
+    $ gIntersect FileA.txt FileB.txt
+    world
+
+
+#Memory
+**gIntersect** reads the contents of **FileA.txt** into memory then compares it with **FileB.txt** . So performance will deteriorate for very large **FileA.txt** file.
+
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -10,16 +11,81 @@ var howToUse = `usage : intersect [File A] [File B]
 Prints out the lines that are both in [File A] and [File B]`
 
 //Exit with error message
-func exit(msg string) {
+func exit(msg interface{}) {
 	fmt.Fprintln(os.Stderr, msg)
 	os.Exit(1)
+}
+
+//Get the Word Map
+func getWordMap(fileName string) (map[string]bool, error) {
+
+	//This map will load every word as key and true as value
+	var wordMap = make(map[string]bool)
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// word by word parsing
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		wordMap[scanner.Text()] = true
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return wordMap, err
+}
+
+func intersect(wordMap map[string]bool, inputFile string) error {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		word := scanner.Text()
+		if wordMap[word] {
+			fmt.Println(word)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
 
 	flag.Parse()
 
+	//Check two file included or not
 	if len(flag.Args()) < 2 {
 		exit(howToUse)
+	}
+
+	wordMap, err := getWordMap(flag.Arg(0))
+
+	if err != nil {
+		exit(err)
+	}
+
+	if err := intersect(wordMap, flag.Arg(1)); err != nil {
+		exit(err)
 	}
 }
